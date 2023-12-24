9445dc8bb8e2b809042d8d51d9666a7a81169fa2
Prathmesh203 <117448429+Prathmesh203@users.noreply.github.com>
2023-12-24 09:39:35 +0530

    Create first.sol

---
+++ first.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Revert_Money {
+    uint256 public Coin;
+
+    function Define_Curr(uint256 MY_Coin) public {
+        // Require is used to validate a condition
+        require(MY_Coin > 1800, "Value must be greater than 1800");
+
+        // Using assert() to ensure an internal condition
+        //and to check the invariants
+        assert(MY_Coin < 8000);
+
+        // Setting the value
+        if(MY_Coin == 6650){
+            // Using revert() to revert the transaction
+        revert("This operation is not allowed"); 
+        }
+        Coin = MY_Coin;
+        
+        
+    }
+
+    
+}
